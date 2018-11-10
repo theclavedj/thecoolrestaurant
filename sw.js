7bb8a6e4dc6eee1ca0c3c27aa544e20d68612328
Miguel Jimenez <m-a-jimenez@hotmail.com>
2018-11-10 19:06:37 +0200

    Added cache v1

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
+var CACHE_NAME = 'my-cache-v1';
+var urlsToCache = [
+  '/',
+];
+
 self.addEventListener('install', function(event) {
   //perform install steps
+  event.waitUntill(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      console.log('loaded cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
