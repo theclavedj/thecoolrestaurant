552cd4c1a54100d824fc7b656feecfb1c3c1276a
Miguel Jimenez <m-a-jimenez@hotmail.com>
2018-11-11 15:33:50 +0200

    Corrected misspelling for ARIA + up button 2d page

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -166,4 +166,22 @@ class DBHelper {
       marker.addTo(newMap);
     return marker;
   }
+  //scroll up: info gather from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+      if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+          document.getElementById("go-up").style.display = "block";
+      } else {
+          document.getElementById("go-up").style.display = "none";
+      }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 }
